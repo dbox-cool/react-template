acbc5d4f0dae1d0b53e1625aade20fd2413dc50e
Gabriel Belisario <gabrielbelisariof@gmail.com>
2024-03-09 10:22:05 -0400

    firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+
+const config = {};
+
+const firebaseApp = firebase.initializeApp(config)
+
+export default firebaseApp
